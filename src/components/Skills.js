a58ad49d48cd8e54ec1410638e373038cbfb28e2
Srijit Kundu <srijit2004@gmail.com>
2023-12-10 01:22:17 +0530

    Fix layout and add Skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+   <>
+    <h2 className='text-lg font-dark py-8'>Skills</h2>
+    <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-12'>
+        
+    </div>
+   </>
+        
+   
+        
+  )
+}
+
+export default Skills
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
 import profilePic from '../../public/images/profile/self.png'
+import Skills from '@/components/Skills'
+
 
 const about = () => {
     return (
@@ -14,28 +16,29 @@ const about = () => {
             </Head>
             <main className='flex w-full flex-col items-center justify-center'>
                 <Layout className='pt-16'>
-                    <AnimatedText text="Passion Fuels Purpose!"  className='mb-4'/>
-                    <div className='grid 
-
-                    w-full grid-cols-8 gap-16'>
-                        <div className='flex flex-col mx-24 items-start justify-start col-span-3 '>
-                            <h2 className='text-lg  font-bold uppercase text-dark/75 mb-4'>Biogrpahy</h2>
+                    <AnimatedText text="Passion Fuels Purpose!" />
+                    <div className='grid grid-cols-8 gap-16 w-full'>
+                        <div className='col-span-3 flex flex-col items-start justify-start'>
+                            <h2 className='mb-4 text-1g font-bold uppercase
+• text-dark/75 ' >Biography</h2>
                             <p className='font-medium '>
                                 Hello, I'm Srijit Kundu, a machine learning engineer with a knack for crafting intelligent and impactful solutions. With 4 years of hands-on experience, I thrive on turning complex challenges into innovative outcomes.
                             </p>
                             <p className='font-medium my-4 ' >
-                            I believe that machine learning goes beyond algorithms – it's about addressing real-world problems and creating solutions that resonate. Whether I'm developing predictive models, optimizing algorithms, or diving into deep learning, my commitment to excellence and user-centric solutions remains unwavering.
+                                I believe that machine learning goes beyond algorithms – it's about addressing real-world problems and creating solutions that resonate. Whether I'm developing predictive models, optimizing algorithms, or diving into deep learning, my commitment to excellence and user-centric solutions remains unwavering.
                             </p>
                             <p className='font-medium '>
-                            In the realm of artificial intelligence, I'm dedicated to bringing my skills and passion to shape the future. Let's collaborate on your next project, where data meets innovation!"
+                                In the realm of artificial intelligence, I'm dedicated to bringing my skills and passion to shape the future. Let's collaborate on your next project, where data meets innovation!"
                             </p>
                         </div>
 
                         <div className='col-span-3 relative h-auto rounded-2xl border-2 border-solid border-dark
                         bg-light p-8 '>
+                        <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
                             <Image src={profilePic} alt='Srijit' className='w-full h-auto rounded-2xl'></Image>
                         </div>
                     </div>
+                   <Skills/>
                 </Layout>
 
 
