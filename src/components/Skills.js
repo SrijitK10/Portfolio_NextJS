c16c82944c12d720b7b9df5a8082e6a69a2be57a
Srijit Kundu <srijit2004@gmail.com>
2023-12-19 15:11:12 +0530

    Refactor Skills component styles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -54,9 +54,9 @@ const SkillCard = ({ src, alt, title }) => {
       onHoverEnd={handleHoverEnd}
       animate={controls}
     >
-      <div className='mx-auto relative h-40 w-40 rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
-        <div className='absolute top-0 -right-3 -z-10 w-[109%] h-[104%] rounded-[1.5rem] bg-dark dark:bg-light' />
-        <Image src={src} alt={alt} className='w-full h-full rounded-2xl object-cover' />
+      <div className='mx-auto relative h-40 w-40 rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xs:h-32 xs:w-32 xs:p-4'>
+        <div className='absolute top-0 -right-3 -z-10 w-[109%] h-[104%] rounded-[1.5rem] bg-dark dark:bg-light xs:flex xs:items-center xs:justify-between' />
+        <Image src={src} alt={alt} className='w-full h-full rounded-2xl object-cover xs:w-[75%]  xs:h-[75%] xs:flex xs:items-center xs:justify-center xs:mx-auto md:' />
         <div className='mt-2 text-sm font-semibold'>{title}</div>
       </div>
     </motion.div>
@@ -69,7 +69,7 @@ const Skills = () => {
     <>
       <h2 className='font-bold text-8xl mt-64
        w-full text-center md:text-6xl md:mt-32'>Skills</h2>
-      <div className='w-full grid grid-cols-6 sm:grid-cols-2 gap-4 text-center py-8 px-4 sm:px-12 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4'>
+      <div className='w-full grid grid-cols-6 sm:grid-cols-2 gap-4 text-center py-8 px-4 sm:px-12 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 xs:!gap-x-6'>
         <SkillCard src={c} alt='C' title='C' className='hover:scale-105 duration:400'/>
         <SkillCard src={cpp} alt='C++' title='C++' />
         <SkillCard src={java} alt='Java' title='Java' />
